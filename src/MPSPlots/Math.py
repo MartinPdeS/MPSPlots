6b05b538d3bdedd836ce25ed777ab69f879a1432
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2022-11-01 21:48:51 -0400

    added Math annd Utils

---
+++ src/MPSPlots/Math.py
@@ -0,0 +1,194 @@
+import numpy
+from scipy import ndimage
+import re
+
+from MPSPlots.Utils import ToList
+
+
+class Angle(object):
+
+    def __init__(self, input, Unit='degree'):
+        if Unit.lower() == 'degree':
+            self.Degree = ToList([i if i is not None else numpy.nan for i in input])
+            self.Radian = ToList([numpy.deg2rad(i) if i is not None else numpy.nan for i in input])
+
+        if Unit.lower() == 'radian':
+            self.Radian = ToList([i if i is not None else numpy.nan for i in input])
+            self.Degree = ToList([numpy.rad2deg(i) if i is not None else numpy.nan for i in input])
+
+        if len(self.Degree) == 1:
+            self.Degree = ToList([self.Degree[0]])
+            self.Radian = ToList([self.Radian[0]])
+
+
+def Deg2Rad(Value):
+    if Value is None: 
+        return numpy.nan
+    else: 
+        return numpy.deg2rad(Value)
+
+
+def Norm(Scalar):
+    return numpy.sqrt(numpy.sum(numpy.abs(Scalar)**2))
+
+
+def Normalize(Scalar):
+    norm = Norm(Scalar)
+    if norm == 0 or numpy.isnan(norm):
+        return [0, 0, 0]
+    else:
+        return Scalar / norm
+
+
+def InterpFull(Meshes, Scalar, Shape):
+
+    Phi, Theta = numpy.mgrid[-numpy.pi / 2:numpy.pi / 2:complex(Shape[0]),
+                          -numpy.pi:numpy.pi:complex(Shape[1])]
+
+    Scalar = interp_at(Meshes.Phi.Radian,
+                       Meshes.Theta.Radian,
+                       Scalar.astype(numpy.complex).flatten(),
+                       Phi.flatten(),
+                       Theta.flatten(),
+                       algorithm='linear',
+                       extrapolate=True)
+
+    return Scalar.reshape(Shape), Phi, Theta
+
+
+def RescaleComplex(Input, Num):
+    Scale = Num/Input.shape[0]
+    InputReal = ndimage.interpolation.zoom(input=Input.real, zoom=(Scale), order=2)
+    InputImag = ndimage.interpolation.zoom(input=Input.imag, zoom=(Scale), order=2)
+    return InputReal + 1j * InputImag
+
+
+def RotateComplex(Input, Rotation):
+    InputReal = ndimage.rotate(Input.real, Rotation, reshape=False)
+    InputImag = ndimage.rotate(Input.imag, Rotation, reshape=False)
+    return InputReal + 1j * InputImag
+
+
+def Angle2Direct(AngleVec: numpy.ndarray, k: float,) -> numpy.ndarray:
+
+    RadSpace = numpy.deg2rad(AngleVec)
+
+    FourierSpace = numpy.sin(RadSpace) * k / (2 * numpy.pi)
+
+    fourier_unit = (FourierSpace[1] - FourierSpace[0]).__abs__()
+
+    DirectSpace = numpy.fft.fftshift(numpy.fft.fftfreq(AngleVec.shape[0], d=fourier_unit))
+
+    return DirectSpace
+
+
+def Direct2Angle(DirectVec: numpy.ndarray, k: float) -> numpy.ndarray:
+
+    direct_unit = (DirectVec[1] - DirectVec[0]).__abs__()
+
+    FourierSpace = numpy.fft.fftshift(numpy.fft.fftfreq(DirectVec.shape[0], d=direct_unit))
+
+    AngleVec = numpy.arcsin(2 * numpy.pi * FourierSpace / k) # conversion spatial frequency to angular space
+
+    if numpy.isnan(AngleVec).any():
+        raise Exception("Magnification too large.")
+
+    return AngleVec * 180 / numpy.pi
+
+
+def NA2Angle(NA: float) -> numpy.ndarray:
+    if NA <= 1.0: 
+        return Angle(numpy.arcsin(NA), Unit='Radian')
+    if NA >= 1.0: 
+        return Angle(numpy.arcsin(NA - 1) + numpy.pi / 2, Unit='Radian')
+
+
+def Direct2spherical(X, Y, MaxAngle):
+    Z = 50 / numpy.tan(MaxAngle)
+
+    _, Phi, Theta = Cart2Sp(X, Y, X * 0 + Z)
+
+    return Phi, Theta
+
+
+def Direct2Angle(X, Y, MaxAngle):
+    MaxZ = numpy.max(X) / numpy.cos(MaxAngle)
+
+
+def AngleUnit2DirectUnit(Angle, k):
+    FourierSpace = numpynpnumpy.sin(Angle) * k / (2 * numpy.pi)
+
+    fourier_unit = (FourierSpace[1] - FourierSpace[0]).__abs__()
+
+    DirectSpace = numpy.fft.fftshift(numpy.fft.fftfreq(Angle.shape[0], d=fourier_unit))
+
+    return DirectSpace
+
+
+def Cart2Sp(x, y, z):
+    R = numpy.sqrt(x**2 + y**2 + z**2)
+    Phi = numpy.arcsin(z/R)
+    Theta = numpy.arctan2(y, x)
+    return R, Phi, Theta
+
+
+def Sp2Cart(Phi, Theta, R=None):
+    R = R if R is not None else Phi * 0 + 1
+    x = R * numpy.cos(Phi) * numpy.cos(Theta)
+    y = R * numpy.cos(Phi) * numpy.sin(Theta)
+    z = R * numpy.sin(Phi)
+    return x, y, z
+
+
+def RotateY(Phi, Theta, Angle):
+    x, y, z = Sp2Cart(Phi=Phi, Theta=Theta)
+    xp = x * numpy.cos(Angle) + z * numpy.sin(Angle)
+    yp = y
+    zp = z * numpy.cos(Angle) - x * numpy.sin(Angle)
+    return Cart2Sp(x=xp, y=yp, z=zp)
+
+
+def RotateZ(Phi, Theta, Angle):
+    x, y, z = Sp2Cart(Phi=Phi, Theta=Theta)
+    xp = x * numpy.cos(Angle) - y * numpy.sin(Angle)
+    yp = x * numpy.sin(Angle) + y * numpy.cos(Angle)
+    zp = z
+    return Cart2Sp(x=xp, y=yp, z=zp)
+
+
+def RotateX(Phi, Theta, Angle):
+    x, y, z = Sp2Cart(Phi=Phi, Theta=Theta)
+
+    xp = x
+    yp = y * numpy.cos(Angle) - z * numpy.sin(Angle)
+    zp = y * numpy.sin(Angle) + z * numpy.cos(Angle)
+    return Cart2Sp(x=xp, y=yp, z=zp)
+
+
+def FormatStr(function):
+    def wrapped(*args, **kwargs):
+        args = (re.sub(r"\s+", "", arg.lower()) if isinstance(arg, str) else arg for arg in args)
+
+        kwargs = {k: re.sub(r"\s+", "", v.lower()) if isinstance(v, str) else v for k, v in kwargs.items()}
+
+        return function(*args, **kwargs)
+    return wrapped
+
+
+def GetSphericalMesh(Sampling, MaxAngle):
+
+    x, y = numpy.mgrid[-50: 50: complex(Sampling), -50: 50: complex(Sampling)]
+    z = 50 / numpy.tan(MaxAngle)
+    _, theta, phi = Cart2Sp(x, y, x * 0 + z)
+
+    return phi, theta
+
+
+def Angle2Jones(Delta):
+    val = numpy.exp(1j * Delta) * 2
+    JonesVector = numpy.array([1, val])
+    Norm = (numpy.sqrt(1 + numpy.abs(val)**2))
+    return JonesVector / Norm
+
+
+#-
--- src/MPSPlots/Render3D.py
+++ src/MPSPlots/Render3D.py
@@ -1,23 +1,29 @@
-
-#!/usr/bin/env python
+#   !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pyvista, logging, numpy
+import pyvista, numpy
+
 
 class Scene3D:
     
-    def __init__(self, **kwargs):
-        self.Figure = pyvista.Plotter(theme=pyvista.themes.DocumentTheme(), **kwargs)
+    def __init__(self, shape: tuple = (1, 1), 
+                       unit_size: tuple = (800, 800), 
+                       window_size: tuple = None, **kwargs):
 
+        if window_size is None:
+            window_size = (unit_size[1] * shape[1], unit_size[0] * shape[0])
 
-    def Add_Unstructured(self, Coordinate: numpy.ndarray, Scalar: numpy.ndarray=None, Plot: tuple=(0,0), **kwargs):
+        self.Figure = pyvista.Plotter(theme=pyvista.themes.DocumentTheme(), 
+                                      window_size=window_size, 
+                                      shape=shape, **kwargs)
+
+    def Add_Unstructured(self, Coordinate: numpy.ndarray, Scalar: numpy.ndarray = None, Plot: tuple = (0, 0), **kwargs):
         self.Figure.subplot(*Plot)
         Coordinate = numpy.array(Coordinate).T
         Points = pyvista.wrap(Coordinate)
         self.Figure.add_points(Points, scalars=Scalar, point_size=20, render_points_as_spheres=True, **kwargs)
 
-
-    def Add_Mesh(self, Coordinate: numpy.ndarray, Plot: tuple=(0,0), **kwargs):
+    def Add_Mesh(self, Coordinate: numpy.ndarray, Plot: tuple = (0, 0), **kwargs):
         self.Figure.subplot(*Plot)
         mesh = pyvista.StructuredGrid(*Coordinate)
 
@@ -25,43 +31,38 @@ class Scene3D:
 
         return self.Figure
 
-
-    def Add_theta_vector_field(self, Plot, R=[1.03/2]):
+    def Add_theta_vector_field(self, Plot, Radius=1.03 / 2):
         self.Figure.subplot(*Plot)
         theta = numpy.arange(0, 360, 10)
         phi = numpy.arange(180, 0, -10)
 
+        Theta_vector = numpy.stack([i.transpose().swapaxes(-2, -1).ravel("C") for i in pyvista.transform_vectors_sph_to_cart(theta, phi, Radius, [1], [0], [0])], axis=1)
 
-        Theta_vector = numpy.stack( [ i.transpose().swapaxes(-2, -1).ravel("C") for i in pyvista.transform_vectors_sph_to_cart( theta, phi, R, [1], [0], [0] ) ], axis=1 )
-
-        Spherical_Vector = pyvista.grid_from_sph_coords(theta, phi, R)
+        Spherical_Vector = pyvista.grid_from_sph_coords(theta, phi, Radius)
 
         Spherical_Vector.point_data["Theta"] = Theta_vector * 0.1
 
         self.Figure.add_mesh(Spherical_Vector.glyph(orient="Theta", scale="Theta", tolerance=0.005), color='k')
 
-
-    def Add_phi_vector_field(self, Plot, R=[1.03/2]):
+    def Add_phi_vector_field(self, Plot, Radius=1.03 / 2):
         self.Figure.subplot(*Plot)
         theta = numpy.arange(0, 360, 10)
         phi = numpy.arange(180, 0, -10)
 
-        Phi_vector = numpy.stack( [ i.transpose().swapaxes(-2, -1).ravel("C") for i in pyvista.transform_vectors_sph_to_cart( theta, phi, R, [0], [1], [0] ) ], axis=1 )
+        Phi_vector = numpy.stack([i.transpose().swapaxes(-2, -1).ravel("C") for i in pyvista.transform_vectors_sph_to_cart(theta, phi, Radius, [0], [1], [0])], axis=1)
 
-
-        Spherical_Vector = pyvista.grid_from_sph_coords(theta, phi, R)
+        Spherical_Vector = pyvista.grid_from_sph_coords(theta, phi, Radius)
 
         Spherical_Vector.point_data["Phi"] = Phi_vector * 0.1
 
-        p = self.Figure.add_mesh(Spherical_Vector.glyph(orient="Phi", scale="Phi", tolerance=0.005), color='k')
+        self.Figure.add_mesh(Spherical_Vector.glyph(orient="Phi", scale="Phi", tolerance=0.005), color='k')
 
-
-    def Add_r_vector_field(self, Plot, R=[1.03/2]):
+    def Add_r_vector_field(self, Plot, R=[1.03 / 2]):
         self.Figure.subplot(*Plot)
         theta = numpy.arange(0, 360, 10)
         phi = numpy.arange(180, 0, -10)
 
-        R_vector = numpy.stack( [ i.transpose().swapaxes(-2, -1).ravel("C") for i in pyvista.transform_vectors_sph_to_cart( theta, phi, R, [0], [0], [1] ) ], axis=1 )
+        R_vector = numpy.stack([i.transpose().swapaxes(-2, -1).ravel("C") for i in pyvista.transform_vectors_sph_to_cart(theta, phi, R, *[0, 0, 1])], axis=1)
 
         Spherical_Vector = pyvista.grid_from_sph_coords(theta, phi, R)
 
@@ -69,24 +70,18 @@ class Scene3D:
 
         p.add_mesh(Spherical_Vector.glyph(orient="R", scale="R", tolerance=0.005), color='k')
 
-
-
-    def __add_unit_sphere__(self, Plot: tuple=(0,0), **kwargs):
+    def __add_unit_sphere__(self, Plot: tuple = (0, 0), **kwargs):
         self.Figure.subplot(*Plot)
         sphere = pyvista.Sphere(radius=1)
         self.Figure.add_mesh(sphere, opacity=0.3)
 
-
-    def __add_axes__(self, Plot: tuple=(0,0)):
+    def __add_axes__(self, Plot: tuple = (0, 0)):
         self.Figure.subplot(*Plot)
         self.Figure.add_axes_at_origin(labels_off=True)
 
-
-    def __add__text__(self, Plot: tuple=(0,0), Text='', **kwargs):
+    def __add__text__(self, Plot: tuple = (0, 0), Text='', **kwargs):
         self.Figure.subplot(*Plot)
         self.Figure.add_text(Text, **kwargs)
 
-
-    def Show(self, SaveDir: str=None):
+    def Show(self, SaveDir: str = None):
         self.Figure.show(screenshot=SaveDir)
-
