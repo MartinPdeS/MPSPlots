e617e824fe5fd41c47deebbd9e88f71c4e0545a4
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-08-17 23:08:11 -0400

    update

--- MPSPlots/styles/__init__.py
+++ MPSPlots/styles/__init__.py
@@ -5,6 +5,13 @@ from MPSPlots.tools.directories import style_directory
 import matplotlib.pyplot as plt
 
 
+
+mps = style_directory.joinpath('mps_plot.mplstyle')
+
+new_age = style_directory.joinpath('new_age.mplstyle')
+
+gg_plot = style_directory.joinpath('ggplot.mplstyle')
+
 def use_mpsplots_style():
     plt.style.use(style_directory.joinpath('mps_plot.mplstyle'))
 
@@ -20,8 +27,5 @@ def use_ggplot_style():
 def use_default_style():
     plt.rcParams["mathtext.fontset"] = "dejavuserif"
     plt.rcParams["font.family"] = "serif"
-
-
-use_mpsplots_style()  # default
 
 # -
