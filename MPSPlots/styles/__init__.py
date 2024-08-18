dc5ed3f5c0abbbf4caf1f005bcca92f9ed269db1
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2024-08-17 23:27:54 -0400

    small update

--- MPSPlots/styles/__init__.py
+++ MPSPlots/styles/__init__.py
@@ -4,8 +4,6 @@
 from MPSPlots.tools.directories import style_directory
 import matplotlib.pyplot as plt
 
-
-
 mps = style_directory.joinpath('mps_plot.mplstyle')
 
 new_age = style_directory.joinpath('new_age.mplstyle')
@@ -13,15 +11,14 @@ new_age = style_directory.joinpath('new_age.mplstyle')
 gg_plot = style_directory.joinpath('ggplot.mplstyle')
 
 def use_mpsplots_style():
-    plt.style.use(style_directory.joinpath('mps_plot.mplstyle'))
-
+    plt.style.use(mps)
 
 def use_new_age_style():
-    plt.style.use(style_directory.joinpath('new_age.mplstyle'))
+    plt.style.use(new_age)
 
 
 def use_ggplot_style():
-    plt.style.use(style_directory.joinpath('ggplot.mplstyle'))
+    plt.style.use(gg_plot)
 
 
 def use_default_style():
